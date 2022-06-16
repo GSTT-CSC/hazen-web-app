aa55a745bd8a894f616636de68fe5bba46194fad
Tom Roberts <t.roberts@kcl.ac.uk>
2022-06-16 11:52:55 +0100

    Adds delete_acq() function – removes acquisition db entry

--- app/main/routes.py
+++ app/main/routes.py
@@ -90,3 +90,16 @@ def explore():
 
     return render_template("index.html", title='Explore', acquisitions=acquisitions.items, next_url=next_url,
                            prev_url=prev_url)
+
+
+@bp.route('/user/<username>/<acquisition_uuid>/')
+@login_required
+def delete_acq(username, acquisition_uuid):
+    user = User.query.get(current_user.get_id())
+    acquisition = Acquisition.query.filter_by(id=acquisition_uuid, user_id=user.id)
+
+    # remove db entry
+    acquisition.delete()
+    db.session.commit()
+
+    return redirect(request.referrer)
