800f87b273a4f0ae724e2d86a5b5f06939d6c042
Laurence Jackson <laurence.jackson@kcl.ac.uk>
2022-07-19 16:07:32 +0100

    change default report string

--- hazenlib/HazenTask.py
+++ hazenlib/HazenTask.py
@@ -6,7 +6,7 @@ from pydicom import dcmread
 
 class HazenTask:
 
-    def __init__(self, data_paths: list, report: bool = False, report_path: str = 'report.txt'):
+    def __init__(self, data_paths: list, report: bool = False, report_path: str = 'hazen_report'):
         self.data_paths = data_paths
         self.report: bool = report
         self.report_path: str = report_path
