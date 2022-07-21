a31e34216927784f225dae2490d8a3680fde4ab2
Tom Roberts <t.roberts@kcl.ac.uk>
2022-07-21 11:23:59 +0100

    Testing Heroku: use REDIS_URL as celery broker and add redis to req.txt

--- config.py
+++ config.py
@@ -26,10 +26,11 @@ class Config:
     DROPZONE_ALLOWED_FILE_CUSTOM = True
 
     # variables when running locally
-    # CELERY_BROKER_URL = 'amqp://localhost'  # for RabbitMQ or 'redis://localhost:6379/0' for Redis
-    # CELERY_RESULT_BACKEND = 'rpc://'  # for RabbitMQ or 'redis://localhost:6379/0' for Redis
+    # CELERY_BROKER_URL = 'amqp://localhost'  # for RabbitMQ
+    # CELERY_RESULT_BACKEND = 'rpc://'  # for RabbitMQ
 
     # variables when running remotely
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-    
+    # CELERY_BROKER_URL = 'redis://localhost:6379/0'  # for Redis
+    # CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'  # for Redis
+    CELERY_BROKER_URL = os.environ.get('REDIS_URL')  # for Redis
+    CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')  # for Redis
