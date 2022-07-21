23e2320c6224add344d4ba76960195a34356fddb
sophie22 <rzsozso@gmail.com>
2022-07-21 02:01:56 +0100

    ingest files into new db tables

--- app/main/routes.py
+++ app/main/routes.py
@@ -25,7 +25,7 @@ def before_request():
 # Overview of process tasks that can be performed
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
-@login_required
+# @login_required
 def index():
     # list available tasks that can be performed
     tasks = Task.query.all()
@@ -33,35 +33,30 @@ def index():
     return render_template('index.html', title='Home', tasks=tasks)
 
 
-class SeriesExistsError(Exception): pass
+class ImageExistsError(Exception): pass
 
 
-# Dashboard/workbench?
+# Workbench?
 # authenticated users can overview and perform tasks/analysis on uploaded files 
-@bp.route('/dashboard/', methods=['GET', 'POST'])
+@bp.route('/workbench/', methods=['GET', 'POST'])
 @login_required
-def dashboard():
-    # locate user in db by username
-    user = User.query.filter_by(username=current_user.username).first_or_404()
-    # list available tasks that can be performed
+def workbench():
+    # Display available tasks that can be performed
     tasks = Task.query.all()
 
-    # Display list of available image Series
+    # Display available image Series
     page = request.args.get('page', 1, type=int)
-    acquisitions = user.acquisitions.order_by(Series.created_at.desc()).paginate(
+    acquisitions = Series.query.filter_by(user_id=current_user.id).paginate(
         page, current_app.config['ACQUISITIONS_PER_PAGE'], False)
-    next_url = url_for('main.dashboard', page=acquisitions.next_num) \
+    # user.series.order_by(Series.created_at.desc()).paginate(
+    #     page, current_app.config['ACQUISITIONS_PER_PAGE'], False)
+    next_url = url_for('main.workbench', page=acquisitions.next_num) \
         if acquisitions.has_next else None
-    prev_url = url_for('main.dashboard', page=acquisitions.prev_num) \
+    prev_url = url_for('main.workbench', page=acquisitions.prev_num) \
         if acquisitions.has_prev else None
 
     if request.method == 'POST': # form.validate_on_submit()
         for key, file in request.files.items():
-            """
-            for file in files:
-                if file and allowed_file(file.filename):
-            """
-
             if key.startswith('file'):
                 filename = secure_filename(file.filename)
                 secure_path = os.path.join(current_app.config['UPLOADED_PATH'], filename)
@@ -69,53 +64,72 @@ def dashboard():
 
                 try:
                     filesystem_dir = ingest(secure_path)
-                except SeriesExistsError:
+                except ImageExistsError:
                     os.remove(secure_path)
-                    flash('SeriesInstanceUID already exists!')
-                    return redirect(url_for('main.dashboard'))
+                    flash('Images have already been uploaded!', 'danger')
+                    return redirect(url_for('main.workbench'))
 
                 permanent_path = os.path.join(filesystem_dir, filename)
-
                 shutil.move(secure_path, permanent_path)
-                flash('Upload success!')
+                flash('Files uploaded successfully!', 'success')
+        return redirect(url_for('main.workbench'))
 
-        return redirect(url_for('main.dashboard'))
-
-    return render_template('user.html', title='Acquisitions', # , tasks=tasks
+    return render_template('workbench.html', title='Workbench', tasks=tasks,
         acquisitions=acquisitions.items, next_url=next_url, prev_url=prev_url)
 
 
-# Upload acquisitions and parse metadata from DICOM header
+# Upload images one at a time and parse metadata from DICOM header
 def ingest(file_path):
     try:
+        # Load in the DICOM header into a pydicom Dataset
         dcm: pydicom.Dataset = pydicom.read_file(file_path, stop_before_pixels=True)
-        # TODO: parse additional fields from DICOM 
-        series_instance_uid = dcm.SeriesInstanceUID
+
+        # Parse the relevant fields into variables
+        series_uid = dcm.SeriesInstanceUID
+        study_uid = dcm.StudyInstanceUID
         description = f"{dcm.StudyDescription}: {dcm.SeriesDescription}"
         filename = os.path.basename(file_path)
-        files = 1
+        image_uid = filename
 
-        # TODO: allow multiple files with the same series ID
-        if Acquisition.query.filter_by(series_instance_uid=series_instance_uid).first():
-            current_app.logger.info('series exists')
-            raise SeriesExistsError(f"UID: {series_instance_uid}")
+        # Ensure that image is not yet in database
+        image_exists = db.session.query(db.exists().where(Image.uid == image_uid)).scalar()
+        if image_exists:
+            current_app.logger.info('Image already exists in database')
+            raise ImageExistsError(f"UID: {image_uid}")
 
-        # Create new row in Acquisitions table
-        acq = Acquisition(series_instance_uid=series_instance_uid,
-                          files=files,
-                          description=description,
-                          filename=filename,
-                          user_id=current_user.get_id(),
-                          dicom_metadata=dcm.to_json_dict()
-                          )
+        # Save the image data into the corresponding tables:
+        # 0. Device:
+        
+        
+        # 1. Study:
+        study_exists = db.session.query(db.exists().where(Study.uid == study_uid)).scalar()
+        if not study_exists:
+            new_study = Study(uid=study_uid, description=dcm.StudyDescription)
+            new_study.save()
+        study_id = Study.query.filter_by(uid=study_uid).first().id
+        print("study id:", study_id)
+        
+        # 2. Series:
+        series_exists = db.session.query(db.exists().where(Series.uid == series_uid)).scalar()
+        if not series_exists:
+            series_time = dcm.SeriesTime.split('.')[0]
+            series_datetime = datetime.strptime("-".join([dcm.SeriesDate,series_time]), '%Y%m%d-%H%M%S')
+            new_series = Series(uid=series_uid, description=dcm.SeriesDescription, user_id=current_user.get_id(), study_id=study_id, series_datetime=series_datetime)
+            new_series.save()
+        series_id = Series.query.filter_by(uid=series_uid).first().id
+        print("series id:", series_id)
 
-        acq.save()
+        # 3. Image:
+        new_image = Image(uid=image_uid, series_id=series_id, filename=filename, header=dcm.to_json_dict())
+        new_image.save()
 
-        # Maybe change location allocation of where uploaded files are stored
-        user = User.query.get(current_user.get_id())
-        directory = os.path.join(current_app.config['UPLOADED_PATH'], user.filesystem_key, acq.filesystem_key)
+        # Commit all changes to the database
+        db.session.commit()
+
+        # Store file in series/image folder
+        series_folder = Series.query.filter_by(uid=series_uid).first().filesystem_key
+        directory = os.path.join(current_app.config['UPLOADED_PATH'], series_folder, new_image.filesystem_key)
         os.makedirs(directory, exist_ok=True)
-        flash('File(s) successfully uploaded')
 
         return directory
 
@@ -139,3 +153,11 @@ def delete_acq(acquisition_uuid):
     db.session.commit()
 
     return redirect(request.referrer)
+
+
+# Reports dashboard
+# Trend monitoring and overview of reports
+@bp.route('/reports/', methods=['GET', 'POST'])
+@login_required
+def reports():
+    return redirect(url_for('main.index'))
