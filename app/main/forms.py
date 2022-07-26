7000869615d8b1c7ec5fa46373189fb2e89c37e0
sophie22 <rzsozso@gmail.com>
2022-07-26 23:27:45 +0100

    create batch select form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,15 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, MultipleFileField, SubmitField
+from wtforms import MultipleFileField, SelectField, SelectMultipleField, RadioField, StringField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Length
 
-# Upload files form
+
+# Upload files
 class ImageUploadForm(FlaskForm):
     image_files = MultipleFileField()
     submit = SubmitField('Upload')
 
 
-# Select task form
+# Select task
 class ProcessTaskForm(FlaskForm):
     task_name = SelectField('Process Task')
     task_variable = StringField('Slice width for SNR measurement')
     submit = SubmitField('Run task')
+
+# Select multiple Series and a Task
+class BatchProcessingForm(FlaskForm):
+    task_name = RadioField()
+    many_series = SelectMultipleField()
+    submit = SubmitField('Run task on selected series')
