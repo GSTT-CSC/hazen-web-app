2f823081b21e4b081b71b62b1a180058d42180e5
sophie22 <sophie22@users.noreply.github.com>
2023-09-07 12:30:40 +0100

    parse additional metadata during ingestion

--- app/main/routes.py
+++ app/main/routes.py
@@ -11,7 +11,7 @@ import pydicom.errors
 from app import db
 from app.main import bp
 from app.main.forms import ImageUploadForm, ProcessTaskForm, BatchProcessingForm
-from app.models import Image, Series, Study, Device, Institution, Task, Report
+from app.models import Image, Series, Study, Device, Task, Report
 
 hazenlib_version = version('hazen')
 
@@ -64,9 +64,9 @@ def ingest(file_path):
                                                 stop_before_pixels=True)
 
         # Parse the relevant fields into variables
+        image_uid = dcm.SOPInstanceUID
         series_uid = dcm.SeriesInstanceUID
         study_uid = dcm.StudyInstanceUID
-        image_uid = dcm.SOPInstanceUID
         description = f"{dcm.StudyDescription}: {dcm.SeriesDescription}"
         filename = os.path.basename(file_path)
 
@@ -76,9 +76,53 @@ def ingest(file_path):
             current_app.logger.info('Image already exists in database')
             raise ImageExistsError(f"UID: {image_uid}")
 
+        # Collect relevant pieces of information from DICOM header
+        # 0020 Study date
+        study_date = dcm.StudyDate
+        # (0008,0020)	DA	Study Date	
+        # (0008,0021)	DA	Series Date	
+        # (0008,0022)	DA	Acquisition Date
+        # 0030 study time
+        study_time = dcm.StudyTime
+        # (0008,0030)	TM	Study Time	
+        # (0008,0031)	TM	Series Time	
+        # (0008,0032)	TM	Acquisition Time
+        institution = dcm.InstitutionName
+        # 0070 manufacturer
+        manufacturer = dcm.Manufacturer
+        # (0008,1090)	LO	Manufacturer's Model Name
+        model = dcm[0x00081090].value
+        station_name = dcm.StationName
+        # 0050 accession number
+        accession_number = dcm.AccessionNumber
+        # print({"institution": institution,
+        #        "manufacturer": manufacturer,
+        #        "model": model,
+        #        "study_date": study_date,
+        #        "study_time": study_time,
+        #        "station_name": station_name,
+        #        "accession_number": accession_number
+        #        })
+
         # Save the image data into the corresponding tables:
+        
         # 0. Device:
-        #TODO parse device and manufacturer information from DICOM to db
+        device_exists = db.session.query(db.exists(
+            ).where(Device.institution == institution
+            ).where(Device.manufacturer == manufacturer
+            ).where(Device.device_model == model
+            ).where(Device.station_name == station_name)).scalar()
+        if not device_exists:
+            new_device = Device(
+                institution=institution, manufacturer=manufacturer,
+                device_model=model, station_name=station_name)
+            new_device.save()
+        device_id = Device.query.where(Device.institution == institution
+            ).where(Device.manufacturer == manufacturer
+            ).where(Device.device_model == model
+            ).where(Device.station_name == station_name).first().id
+        #TODO remove in production
+        print("device id:", device_id)
         
         # 1. Study:
         study_exists = db.session.query(db.exists().where(Study.uid == study_uid)).scalar()
@@ -94,14 +138,19 @@ def ingest(file_path):
         if not series_exists:
             series_time = dcm.SeriesTime.split('.')[0]
             series_datetime = datetime.strptime("-".join([dcm.SeriesDate,series_time]), '%Y%m%d-%H%M%S')
-            new_series = Series(uid=series_uid, description=dcm.SeriesDescription, user_id=current_user.get_id(), study_id=study_id, series_datetime=series_datetime)
+
+            new_series = Series(
+                uid=series_uid, description=dcm.SeriesDescription, user_id=current_user.get_id(), device_id=device_id,
+                study_id=study_id, series_datetime=series_datetime)
             new_series.save()
         series_id = Series.query.filter_by(uid=series_uid).first().id
         #TODO remove in production
         print("series id:", series_id)
 
         # 3. Image:
-        new_image = Image(uid=image_uid, series_id=series_id, filename=filename, header=dcm.to_json_dict())
+        new_image = Image(
+            uid=image_uid, series_id=series_id, filename=filename,
+            accession_number=accession_number)
         new_image.save()
         #TODO remove in production
         print("new image id:", new_image.id)
--- app/models.py
+++ app/models.py
@@ -70,6 +70,19 @@ class User(UserMixin, Model, SurrogatePK, CreatedTimestampMixin):
         return User.query.get(id)
 
 
+class Task(Model, SurrogatePK, CreatedTimestampMixin):  # Previously "ProcessTask"
+    __tablename__ = "task"
+
+    def __init__(self, **kwargs):
+        db.Model.__init__(self, **kwargs)
+
+    # Column "id" is created automatically by SurrogatePK() from database.py
+    name = db.Column(db.String(100), unique=True)  # TODO: Change from reading hazenlib modules to classes
+
+    # One-to-many relationship
+    reports = db.relationship('Report', back_populates='task')
+
+
 class Image(Model, SurrogatePK, CreatedTimestampMixin):  # Previously "Acquisition"
     __tablename__ = "image"
 
@@ -79,7 +92,8 @@ class Image(Model, SurrogatePK, CreatedTimestampMixin):  # Previously "Acquisiti
     # Column "id" is created automatically by SurrogatePK() from database.py
     uid = db.Column(db.String(100))  # DICOM SOP Instance UID (0008,0018)
     filename = db.Column(db.String(200))
-    header = db.Column(JSONB)  # DICOM Header
+    accession_number = db.Column(db.String(100))  # DICOM Accession Number (0008,0050)
+    # header = db.Column(JSONB)  # DICOM Header
     series_id = db.Column(db.ForeignKey('series.id'))
 
     # Many-to-one relationships
@@ -113,7 +127,6 @@ class Series(Model, SurrogatePK, CreatedTimestampMixin):
     """
     user_id = db.Column(db.ForeignKey('user.id'))
     device_id = db.Column(db.ForeignKey('device.id'))
-    institution_id = db.Column(db.ForeignKey('institution.id'))
     study_id = db.Column(db.ForeignKey('study.id'))
 
 
@@ -123,7 +136,6 @@ class Series(Model, SurrogatePK, CreatedTimestampMixin):
 
     # Many-to-one relationship # Child of
     user = db.relationship('User', back_populates='series')
-    institutions = db.relationship('Institution', back_populates='series')
     devices = db.relationship('Device', back_populates='series')
     studies = db.relationship('Study', back_populates='series')
 
@@ -141,46 +153,24 @@ class Study(Model, SurrogatePK, CreatedTimestampMixin):
     # Column "id" is created automatically by SurrogatePK() from database.py
     uid = db.Column(db.String(64))  # DICOM Study UID (0020,000D)
     description = db.Column(db.String(100))  # DICOM Study Description (0008,1030)
+    # study_date = db.Column(db.String(64))  # DICOM Study date (0008,0020)
+    # study_time = db.Column(db.String(64))  # DICOM Study time (0008,0030)
 
     # One-to-many relationships
     series = db.relationship('Series', back_populates='studies')
 
 
-class Task(Model, SurrogatePK, CreatedTimestampMixin):  # Previously "ProcessTask"
-    __tablename__ = "task"
-
-    def __init__(self, **kwargs):
-        db.Model.__init__(self, **kwargs)
-
-    # Column "id" is created automatically by SurrogatePK() from database.py
-    name = db.Column(db.String(100), unique=True)  # TODO: Change from reading hazenlib modules to classes
-
-    # One-to-many relationship
-    reports = db.relationship('Report', back_populates='task')
-
-
-class Institution(Model, SurrogatePK, CreatedTimestampMixin):
-    __tablename__ = "institution"
-
-    def __init__(self, **kwargs):
-        db.Model.__init__(self, **kwargs)
-
-    # Column "id" is created automatically by SurrogatePK() from database.py
-    name = db.Column(db.String(100), unique=True)  # DICOM Institution (0008,0080)
-    # series_id = db.Column(db.ForeignKey('series.id'))
-
-    # One-to-many relationship
-    series = db.relationship('Series', back_populates='institutions')
-
-
 class Device(Model, SurrogatePK, CreatedTimestampMixin):
+    __tablename__ = 'device'
+
     def __init__(self, **kwargs):
         db.Model.__init__(self, **kwargs)
 
     # Column "id" is created automatically by SurrogatePK() from database.py
+    institution = db.Column(db.String(100))  # DICOM Institution (0008,0080)
     manufacturer = db.Column(db.String(100))  # Device Manufacturer (0008,0070)
+    device_model = db.Column(db.String(100))  # Manufacturer's Model Name (0008,1090)
     station_name = db.Column(db.String(100))  # Station Name (0008, 1010)
-    device_model = db.Column(db.String(100))  # Device Model (0008,1090)
 
     # One-to-many relationship
     series = db.relationship('Series', back_populates='devices')
