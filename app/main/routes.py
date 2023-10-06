2afca2f6e9b7d2a918295eb32f2b0bf1500278fc
sophie22 <sophie22@users.noreply.github.com>
2023-10-06 09:48:25 +0100

    create concise view for tasks and results per Series

--- app/main/routes.py
+++ app/main/routes.py
@@ -13,6 +13,7 @@ from app import db
 from app.main import bp
 from app.main.forms import ImageUploadForm, ProcessTaskForm, BatchProcessingForm
 from app.models import Image, Series, Study, Device, Task, Report
+from app.util.im2db_utils import upload_file, locate_image_files
 
 hazenlib_version = version('hazen')
 
@@ -33,143 +34,6 @@ def index():
     tasks = Task.query.all()
 
     return render_template('index.html', title='Home', tasks=tasks)
-
-
-class ImageExistsError(Exception): pass
-
-
-def upload_file(file):
-    filename = secure_filename(file.filename)
-    secure_path = os.path.join(current_app.config['UPLOADED_PATH'], filename)
-    try:
-        file.save(secure_path)
-    except IsADirectoryError:
-        flash("No files were selected", 'info')
-        return redirect(url_for('main.workbench'))
-
-    try:
-        filesystem_dir = ingest(secure_path)
-        permanent_path = os.path.join(filesystem_dir, filename)
-        shutil.move(secure_path, permanent_path)
-        flash(f'{filename} file has been uploaded successfully!', 'success')
-    except ImageExistsError:
-        os.remove(secure_path)
-        flash(f'{filename} file has already been uploaded!', 'danger')
-
-
-# Upload images one at a time and parse metadata from DICOM header
-def ingest(file_path):
-    try:
-        # Load in the DICOM header into a pydicom Dataset
-        dcm = pydicom.read_file(file_path, force=True,
-                                                stop_before_pixels=True)
-
-        # Parse the relevant fields into variables
-        image_uid = dcm.SOPInstanceUID
-        series_uid = dcm.SeriesInstanceUID
-        study_uid = dcm.StudyInstanceUID
-        description = f"{dcm.StudyDescription}: {dcm.SeriesDescription}"
-        filename = os.path.basename(file_path)
-
-        # Ensure that image is not yet in database
-        image_exists = db.session.query(db.exists().where(Image.uid == image_uid)).scalar()
-        if image_exists:
-            current_app.logger.info('Image already exists in database')
-            raise ImageExistsError(f"UID: {image_uid}")
-
-        # Collect relevant pieces of information from DICOM header
-        # 0020 Study date
-        study_date = dcm.StudyDate
-        # (0008,0020)	DA	Study Date	
-        # (0008,0021)	DA	Series Date	
-        # (0008,0022)	DA	Acquisition Date
-        # 0030 study time
-        study_time = dcm.StudyTime
-        # (0008,0030)	TM	Study Time	
-        # (0008,0031)	TM	Series Time	
-        # (0008,0032)	TM	Acquisition Time
-        institution = dcm.InstitutionName
-        # 0070 manufacturer
-        manufacturer = dcm.Manufacturer
-        # (0008,1090)	LO	Manufacturer's Model Name
-        model = dcm[0x00081090].value
-        station_name = dcm.StationName
-        # 0050 accession number
-        accession_number = dcm.AccessionNumber
-        # print({"institution": institution,
-        #        "manufacturer": manufacturer,
-        #        "model": model,
-        #        "study_date": study_date,
-        #        "study_time": study_time,
-        #        "station_name": station_name,
-        #        "accession_number": accession_number
-        #        })
-
-        # Save the image data into the corresponding tables:
-        
-        # 0. Device:
-        device_exists = db.session.query(db.exists(
-            ).where(Device.institution == institution
-            ).where(Device.manufacturer == manufacturer
-            ).where(Device.device_model == model
-            ).where(Device.station_name == station_name)).scalar()
-        if not device_exists:
-            new_device = Device(
-                institution=institution, manufacturer=manufacturer,
-                device_model=model, station_name=station_name)
-            new_device.save()
-        device_id = Device.query.where(Device.institution == institution
-            ).where(Device.manufacturer == manufacturer
-            ).where(Device.device_model == model
-            ).where(Device.station_name == station_name).first().id
-        #TODO remove in production
-        print("device id:", device_id)
-        
-        # 1. Study:
-        study_exists = db.session.query(db.exists().where(Study.uid == study_uid)).scalar()
-        if not study_exists:
-            new_study = Study(uid=study_uid,
-                              description=dcm.StudyDescription,
-                              study_date=study_date)
-            new_study.save()
-        study_id = Study.query.filter_by(uid=study_uid).first().id
-        #TODO remove in production
-        print("study id:", study_id)
-        
-        # 2. Series:
-        series_exists = db.session.query(db.exists().where(Series.uid == series_uid)).scalar()
-        if not series_exists:
-            series_time = dcm.SeriesTime.split('.')[0]
-            series_datetime = datetime.strptime("-".join([dcm.SeriesDate,series_time]), '%Y%m%d-%H%M%S')
-
-            new_series = Series(
-                uid=series_uid, description=dcm.SeriesDescription, user_id=current_user.get_id(), device_id=device_id,
-                study_id=study_id, series_datetime=series_datetime)
-            new_series.save()
-        series_id = Series.query.filter_by(uid=series_uid).first().id
-        #TODO remove in production
-        print("series id:", series_id)
-
-        # 3. Image:
-        new_image = Image(
-            uid=image_uid, series_id=series_id, filename=filename,
-            accession_number=accession_number)
-        new_image.save()
-        #TODO remove in production
-        print("new image id:", new_image.id)
-
-        # Commit all changes to the database
-        db.session.commit()
-
-        # Store file in series/image folder
-        series_folder = Series.query.filter_by(uid=series_uid).first().filesystem_key
-        directory = os.path.join(current_app.config['UPLOADED_PATH'], series_folder)
-        os.makedirs(directory, exist_ok=True)
-
-        return directory
-
-    except Exception as e:
-        raise
 
 
 # Delete or archive series, delete reports
@@ -266,8 +130,11 @@ def workbench():
     if request.method == 'POST':
         if 'file' in request.files.keys():
             # Uploaded by DropZone
-            dropzone_file = request.files['file']
+            dropzone_file = request.files.get('file')
+            if dropzone_file.filename.split(".")[-1].lower() not in current_app.config['ALLOWED_EXTENSIONS']:
+                return 'incorrect file type', 400
             upload_file(dropzone_file)
+
         elif 'submit' in request.form.keys():
             # Batch processing functionality
             if request.form['submit'] == 'Run task on selected series':
@@ -311,18 +178,6 @@ def workbench():
     # , series=series, next_url=next_url, prev_url=prev_url
 
 
-def locate_image_files(filesystem_key, filename=False):
-    # Select files in series folder
-    folder = os.path.join(current_app.config['UPLOADED_PATH'],
-                                    filesystem_key)
-    if filename == True:
-        image_files = os.listdir(folder)
-    else:
-        image_files = [os.path.join(folder, file) for file in os.listdir(folder)]
-    
-    return image_files
-
-
 def create_celery_jobs(user_id, task_name: str, series_ids: list, task_variable=None):
     celery_job_list = []
     from app.tasks import produce_report
@@ -356,11 +211,12 @@ def create_celery_jobs(user_id, task_name: str, series_ids: list, task_variable=
             celery_job_list.append(celery_job)
     return celery_job_list
 
-
-# Select task to be run on (image) series
-@bp.route('/task_selection/<series_id>/', methods=['GET', 'POST'])
+# Series overview
+# Description, existing reports and run new tasks
+@bp.route('/series/<series_id>', methods=['GET', 'POST'])
 @login_required
-def task_selection(series_id):
+def series_view(series_id):
+    user_id = current_user.id
     # Retrieve the Series that was selected
     series = Series.query.filter_by(id=series_id).first_or_404()
 
@@ -371,57 +227,7 @@ def task_selection(series_id):
         form.task_name.choices = [(task.name, task.name) for task in Task.query.all()]
         series_files = Image.query.filter_by(series_id=series_id).count()
 
-        return render_template('task_selection.html', title='Select Task',
-                        form=form, series=series, series_files=series_files,
-                        hazenlib_version=hazenlib_version)
-
-    if request.method == 'POST':
-        # Set off task processing as a Celery job
-        from app.tasks import produce_report
-
-        # Unpack variables from session
-        task_name = request.form['task_name']
-        task_variable = request.form['task_variable']
-        user_id = session['current_user_id']
-
-        # Select files to perform task on
-        folder = os.path.join(current_app.config['UPLOADED_PATH'],
-                                        series.filesystem_key)
-        image_files = [os.path.join(folder, file) for file in os.listdir(folder)]
-
-        # Ensure that appropriate number of files were selected
-        series_files = Image.query.filter_by(series_id=series.id).count()
-        if len(image_files) != series_files:
-            raise Exception('Number of files in directory is not equal to expected!')
-
-        # Set off task processing as a Celery job
-        current_app.logger.info(f"Performing {task_name} task on {series.description}")
-        celery_job = produce_report.delay(
-            user_id=user_id, series_id=series.id, task_name=task_name,
-            image_files=image_files, slice_width=task_variable)
-
-        current_app.logger.info(f"Task performed successfully! \n")
-        flash(f'Completed {task_name} measurement!', 'info')
-
-        # Passing task and series information to next page via Session dict
-        session['task_name'] = task_name
-        session['series_id'] = series_id
-
-        return redirect(url_for('main.result'))
-
-
-# Select task to be run on (image) series
-@bp.route('/result/', methods=['GET', 'POST'])
-@login_required
-def series_results():
-    user_id = current_user.id
-    # If coming from the Workbench with a series_id specified,
-    # then display reports made for that series_id - RESULT
-    if 'series_id' in request.args.keys():
-        # Get series_id from URL
-        series_id = request.args['series_id']
         # Store information about this series in a dict that can be passed to the html
-        series = Series.query.filter_by(id=series_id).first_or_404()
         series_dict = {
             'description': series.description,
             'series_datetime': series.series_datetime,
@@ -434,7 +240,7 @@ def series_results():
         reports = db.session.query(Report).filter_by(
                         series_id=series_id).order_by(Report.created_at.desc())
         #, user_id=user_id
-        # add user restriction later
+        # TODO: add user restriction later
 
         # if len(reports.all()) < 1:
         #     print("No reports available for this user")
@@ -450,8 +256,8 @@ def series_results():
             # Find report images for series + task
             # directory = os.path.join(current_app.config['UPLOADED_PATH'],
             #                             )
-            image_files = locate_image_files(task_result.filesystem_key, 
-                                                filename=True)
+            image_files = locate_image_files(
+                            task_result.filesystem_key, filename=True)
             # Store values in dict to be displayed
             results_dict[task] = {
                 "measurement": task_result.data,
@@ -461,7 +267,44 @@ def series_results():
                 "width": 100 / len(image_files) -1
             }
 
+        return render_template('series_view.html', title='Series overview',
+                        form=form, series=series_dict, results=results_dict,
+                        hazenlib_version=hazenlib_version)
+
+    if request.method == 'POST':
+        # Set off task processing as a Celery job
+        # Unpack variables from session
+        task_name = request.form['task_name']
+        task_variable = request.form['task_variable']
+        user_id = session['current_user_id']
+        # Select files to perform task on
+        folder = os.path.join(current_app.config['UPLOADED_PATH'],
+                                        series.filesystem_key)
+        image_files = [os.path.join(folder, file) for file in os.listdir(folder)]
+
+        # Ensure that appropriate number of files were selected
+        series_files = Image.query.filter_by(series_id=series.id).count()
+        if len(image_files) != series_files:
+            raise Exception('Number of files in directory is not equal to expected!')
+
+        # Create Celery jobs from processing request
+        current_app.logger.info(f"Performing {task_name} task on {series.description}")
+        celery_job_list = create_celery_jobs(
+            user_id=current_user.id, series_ids=[series.id],
+            task_name=task_name, task_variable=task_variable)
+
+        current_app.logger.info(f"Task performed successfully! \n")
+        flash(f'Completed {task_name} measurement!', 'info')
+
+        # Passing task and series information to next page via Session dict
+        session['task_name'] = task_name
+        session['series_id'] = series_id
+
+        return redirect(url_for('main.series_view', series_id=series_id))
+
+
     return render_template('result.html', title='Result',
+                form=form,
                 series=series_dict, results=results_dict)
 
 
